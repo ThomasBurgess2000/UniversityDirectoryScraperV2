1301e52614d12985cbc4ede0edfd4ed85863102c
Thomas Burgess <thomasburgess2000@gmail.com>
2019-10-30 20:59:28 -0700

    Removed school identifiers
     Per request of my school's Director of Systems and Information Security lol

--- DirectoryScraper.py
+++ DirectoryScraper.py
@@ -13,10 +13,10 @@ password=getpass.getpass()
 
 #LOAD PAGE
 driver=webdriver.Chrome('C:/Users/Thomas/Desktop/chromedriver_win32/chromedriver.exe')
-driver.get("https://www.biola.edu/directory/search?_type=person")
+driver.get("https://www.RANDOMSCHOOL.edu/directory/search?_type=person")
 
 #GO TO LOGIN PAGE
-loginLink=driver.find_element_by_xpath('//*[@id="biolaHeader"]/div/div/a')
+loginLink=driver.find_element_by_xpath('//*[@id="RANDOMSCHOOLHeader"]/div/div/a')
 loginLink.click()
 time.sleep(3)
 
@@ -34,20 +34,20 @@ submitForm.click()
 finalListOfNames=[]
 finalListOfEmails=[]
 finalListOfImageLinks=[]
-BiolaDirectory = {}
+RANDOMSCHOOLDirectory = {}
 counter=0
 
 #SCRAPE NAMES, EMAILS, AND IMAGES
 for t in range(1,371):
     tStr=str(t)
-    driver.get("https://www.biola.edu/directory/search?_type=person&page="+tStr)
+    driver.get("https://www.RANDOMSCHOOL.edu/directory/search?_type=person&page="+tStr)
     #Creates list of current page's names, emails, and image links.
     pageListOfNames=driver.find_elements_by_class_name('title')
     pageListOfEmails=driver.find_elements_by_class_name('contact-info')
     pageListOfImageLinks=driver.find_elements_by_css_selector("div.result_image img")
     i=0
     # Adds the elements of the above lists to the master list of names, emails, and addresses as well as adds them all
-    # to the dictionary 'BiolaDirectory' where the key is the name and the value is a list containing the information
+    # to the dictionary 'RANDOMSCHOOLDirectory' where the key is the name and the value is a list containing the information
     # about the person (so far just email and image link).
 
     # fixedPage variables are the corrected versions of their pageList counterparts (i.e. the strings I want).
@@ -72,7 +72,7 @@ for t in range(1,371):
         image=str(image)
         # This guy is the only wacko in the whole directory who doesn't have a picture and screws the whole thing up
         if (name=="Tom Keene"):
-            image="https://apps.biola.edu/idphotos/d36ec5f30a0cd006d11f2c953ac844fb_medium.jpg"
+            image="https://apps.RANDOMSCHOOL.edu/idphotos/d36ec5f30a0cd006d11f2c953ac844fb_medium.jpg"
             i-=1
         finalListOfImageLinks.append(image)
         fixedPageListOfImages.append(image)
@@ -82,12 +82,12 @@ for t in range(1,371):
     while (i<len(pageListOfEmails)):
         #Frickin idiot Tom
         if (pageListOfNames[i].text=='Tom Keene'):
-            BiolaDirectory['Tom Keene']=['',"https://apps.biola.edu/idphotos/d36ec5f30a0cd006d11f2c953ac844fb_medium.jpg"]
+            RANDOMSCHOOLDirectory['Tom Keene']=['',"https://apps.biola.edu/idphotos/d36ec5f30a0cd006d11f2c953ac844fb_medium.jpg"]
             p-=1
         else:
-            BiolaDirectory[pageListOfNames[i].text]=[fixedPageListOfEmails[i],fixedPageListOfImages[p]]
+            RANDOMSCHOOLDirectory[pageListOfNames[i].text]=[fixedPageListOfEmails[i],fixedPageListOfImages[p]]
         #I don't really utilize the dictionary yet, but if I wanted to add some search functionality this would be the way to do it.
-        x=BiolaDirectory[pageListOfNames[i].text]
+        x=RANDOMSCHOOLDirectory[pageListOfNames[i].text]
         print (pageListOfNames[i].text)
         print (x)
         i+=1
@@ -98,13 +98,13 @@ for t in range(1,371):
 i=0
 p=0
 # Writes names, emails, and image URLs to a text file in the local directory.
-with open('bioladirectory.txt','w+') as output:
+with open('RANDOMSCHOOLdirectory.txt','w+') as output:
     while (i<len(finalListOfEmails)):
         output.write("%s\n" % str(i+1))
         output.write("%s\n" % str(finalListOfNames[i]))
         output.write("%s\n" % str(finalListOfEmails[i]))
         if (str(finalListOfNames[i])=='Tom Keene'):
-            output.write("%s\n" % "https://apps.biola.edu/idphotos/d36ec5f30a0cd006d11f2c953ac844fb_medium.jpg")
+            output.write("%s\n" % "https://apps.RANDOMSCHOOL.edu/idphotos/d36ec5f30a0cd006d11f2c953ac844fb_medium.jpg")
             p-=1
         else:
             output.write("%s\n" % str(finalListOfImageLinks[p]))
